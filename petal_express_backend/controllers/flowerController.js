856addd3024e1ae971e951b3a5ffd3023c1368aa
Simul <simulbista@gmail.com>
2023-06-12 00:29:02 -0400

    finished the CRUD rest api(backend) for the flower entity using express,node & mongodb

--- petal_express_backend/controllers/flowerController.js
+++ petal_express_backend/controllers/flowerController.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 // since all the api methods are async, we need to use either try-catch or .then structure, to avoid using any of them
 // we can alternatively use asyncHandler (simply wrap async in this)
 const asyncHandler = require('express-async-handler')
@@ -10,31 +11,45 @@ const getFlowers = asyncHandler(async (req,res) =>{
     // set this filter from the parameter later
     const filter = {};
     const flowers = await flowerModel.find()
-    .sort({ f_id: 1 }); // Sort by f_id in ascending order
+    .sort({ f_id: 1 }).exec(); // Sort by f_id in ascending order
     res.status(200).json(flowers);
 })
 
 
 //  @route POST /api/flowers
 const addFlower = asyncHandler(async (req,res) =>{
-    // if(!req.body.text){
-    //     res.status(400)
-    //     throw new Error('Please add a text field!')
-    // }
-    // const flowers = await flowerModel.create({
-    //     flowers : req.body
-    // })
-    // res.status(200).json(flowers);
+    const flower = await flowerModel.create(req.body)
+    res.status(200).json(flower);
 })
 
 //  @route PUT /api/flowers/:id
 const updateFlowerById = asyncHandler(async (req,res) =>{
-    res.status(200).json({ message: `Update flower ${req.params.id}` });
+    // converting string id to object id since that is the datatype in mongodb
+    const objectId = new mongoose.Types.ObjectId(req.params.id);
+    const flower = await flowerModel.findById(objectId);
+    // if id not found in the db
+    if(!flower){
+        res.status(400)
+        throw new Error(`The flower with id ${objectId} doesn't exist!`)
+    }
+
+    const updatedFlower = await flowerModel.findByIdAndUpdate(objectId,req.body,{new:true});  //new:true will return the data after the update
+    res.status(200).json(updatedFlower);
 } )
 
 //  @route DELETE /api/flowers/:id
 const deleteFlowerById = asyncHandler(async (req,res) =>{
-    res.status(200).json({ message: `Delete flower ${req.params.id}` });
+     // converting string id to object id since that is the datatype in mongodb
+     const objectId = new mongoose.Types.ObjectId(req.params.id);
+     const flower = await flowerModel.findById(objectId);
+     // if id not found in the db
+     if(!flower){
+         res.status(400)
+         throw new Error(`The flower with id ${objectId} doesn't exist!`)
+     }
+ 
+     const deletedFlower = await flowerModel.findByIdAndDelete(objectId);  //new:true will return the data after the update
+     res.status(200).json(deletedFlower);
 })
 
 module.exports = {getFlowers,addFlower,updateFlowerById,deleteFlowerById}
